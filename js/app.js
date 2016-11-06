db0b901002d108aa3172cb23391428c056e806b3
Megaju <julen_malle@outlook.fr>
2016-11-06 15:30:19 +0100

    function add task ok

--- js/app.js
+++ js/app.js
@@ -16,6 +16,14 @@ app.controller('todoCtrl', function($scope) {
     // Nombres de tâches en cours
     $scope.nbRask = 2;
     
+    // Fonction d'ajout de tâche
+    $scope.addTask = function() {
+        $scope.todos.push({
+            name : $scope.newTask,
+            completed : false
+        })
+    }
+    
     // Fonction de supression des tâches
     $scope.removeTask = function(index) {
         $scope.todos.splice(index, 1); // on retire 1 élément à partir de l'élément lui-même.
