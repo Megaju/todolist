1588ed6ae004dd502b869a419170af7b21c35134
Megaju <julen_malle@outlook.fr>
2016-11-06 16:16:50 +0100

    button all checked

--- js/app.js
+++ js/app.js
@@ -13,10 +13,11 @@ app.controller('todoCtrl', function($scope, filterFilter) {
         }
     ];
     
-    // Nombres de tâches en cours
+    // Nombres de tâches en cours + détecter si tout est coché (voir fonction checkAllTask)
     $scope.$watch('todos', function(){
         $scope.nbTaskUncompleted = filterFilter($scope.todos, {completed:false}).length;
         $scope.nbTaskCompleted = filterFilter($scope.todos, {completed:true}).length;
+        $scope.allChecked = !$scope.nbTaskUncompleted;
     }, true) // on ajoute true en 3e paramètre pour dire qu'on l'observe tout le temps!
     
     // Fonction d'ajout de tâche
@@ -31,6 +32,13 @@ app.controller('todoCtrl', function($scope, filterFilter) {
     // Fonction de supression des tâches
     $scope.removeTask = function(index) {
         $scope.todos.splice(index, 1); // on retire 1 élément à partir de l'élément lui-même.
+    }
+    
+    // Fonction pour cocher toutes les tâches d'un coup
+    $scope.checkAllTask = function(allChecked) {
+        $scope.todos.forEach(function(todo){ // boucle foreach avec AngularJS
+            todo.completed = allChecked;
+        })
     }
     
 });
